02bfa0faadd3d877fb34900e9bd227a02058794f
BERNARD Damien <damien.bernard@epitech.eu>
2020-07-12 15:44:31 +0200

    update(Common): Updating Epitech Headers.

--- BabelCommon/Network/Sockets/ASocket/ASocket.cpp
+++ BabelCommon/Network/Sockets/ASocket/ASocket.cpp
@@ -1,6 +1,6 @@
 /*
 ** EPITECH PROJECT, 2020
-** CPP_babel_2020 (Copyright (c) $USER_NAME.capitalize() on 7/12/20).
+** CPP_babel_2020 (Copyright (c) ENCORPLUPTIT on 7/12/20).
 ** File description:
 ** [ASocket.cpp]: Source file for ASocket feature.
 */
--- BabelCommon/Network/Sockets/ISocket/ISocket.cpp
+++ BabelCommon/Network/Sockets/ISocket/ISocket.cpp
@@ -1,6 +1,6 @@
 /*
 ** EPITECH PROJECT, 2020
-** CPP_babel_2020 (Copyright (c) $USER_NAME.capitalize() on 7/12/20).
+** CPP_babel_2020 (Copyright (c) ENCORPLUPTIT on 7/12/20).
 ** File description:
 ** [ISocket.cpp]: Source file for ISocket feature.
 */
