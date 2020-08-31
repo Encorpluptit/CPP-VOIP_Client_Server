48e8360d612a9dee5f857b305ef58442c2e25528
BERNARD Damien <damien.bernard@epitech.eu>
2020-08-31 21:14:54 +0200

    Update(Database): Adding begining of var to call populate.

--- Bonus/GoBabel/tests/DbManagement.go
+++ Bonus/GoBabel/tests/DbManagement.go
@@ -7,6 +7,13 @@ import (
 	"os"
 )
 
+const (
+	UDam = iota
+	UGreg
+	UArthur
+	UUgo
+)
+
 var TestUsersDatas = []*ent.User{
 	{Login: "damien.bernard@epitech.eu", Password: "1234AB_cd666"},
 	{Login: "gregoire.brasseur@epitech.eu", Password: "1234AB_cd666"},
@@ -14,7 +21,29 @@ var TestUsersDatas = []*ent.User{
 	{Login: "ugo.levi-cescutti@epitech.eu", Password: "1234AB_cd666"},
 }
 
+var TestCallDatas = []*ent.Call{
+	{Edges: ent.CallEdges{Participants: []*ent.User{TestUsersDatas[UDam], TestUsersDatas[UArthur]}}},
+}
+
+var TestConfDatas = []*ent.Conference{
+	{Edges: ent.ConferenceEdges{
+		Users: []*ent.User{TestUsersDatas[UDam], TestUsersDatas[UArthur]},
+		Calls: nil,
+	}},
+	{Edges: ent.ConferenceEdges{}},
+	{Edges: ent.ConferenceEdges{}},
+}
+
 func PopulateDb() {
+	for _, user := range TestUsersDatas {
+		_, err := Database.CreateUser(user)
+		if err != nil {
+			log.Printf("failed creating user: %v\ns", err)
+		}
+	}
+}
+
+func createUsers() {
 	for _, user := range TestUsersDatas {
 		_, err := Database.CreateUser(user)
 		if err != nil {
