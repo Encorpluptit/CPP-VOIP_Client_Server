f88e2a78e6287e89c7fbd5eaafa946215c4701ce
BERNARD Damien <damien.bernard@epitech.eu>
2020-09-23 08:50:43 +0200

    update(CallResponse): Adding more tests.

--- tests/criterion_unit_tests/src/Common/Protocol/CallRequest_Tests.cpp
+++ tests/criterion_unit_tests/src/Common/Protocol/CallRequest_Tests.cpp
@@ -15,6 +15,22 @@ Test(Common, CallResponse_01)
 {
     const std::string sender("ugo");
     const std::string receiver("dam");
+
+    CallResponse test(sender, receiver);
+
+    ASSERT_BOOL(test.encode(), true);
+    ASSERT_BOOL(test.decode_header(), true);
+    ASSERT_BOOL(test.decode_data_infos(), true);
+    ASSERT_BOOL(test.decode_data(), true);
+
+    cr_assert_str_eq(test.getSender(), sender.c_str());
+    cr_assert_str_eq(test.getReceiver(), receiver.c_str());
+}
+
+Test(Common, CallResponse_02)
+{
+    const std::string sender("ugo");
+    const std::string receiver("dam");
     const std::string ip("127.0.0.1");
     const std::string port("5555");
 
@@ -30,3 +46,18 @@ Test(Common, CallResponse_01)
     cr_assert_str_eq(test.getIp(), ip.c_str());
     cr_assert_str_eq(test.getPort(), port.c_str());
 }
+//Test(Common, UserResponse_14)
+//{
+//    const std::string login = std::string(UserResponse::MaxDataSize::Login + 1, '*');
+//    const std::string pass = std::string(UserResponse::MaxDataSize::Password, '*');
+//
+//    cr_assert_throw(
+//        UserResponse test(login, pass),
+//        BabelErrors::UserResponse
+//    );
+//    try {
+//        UserResponse test(login, pass);
+//    } catch (const BabelErrors::UserResponse &e) {
+//        cr_assert_str_eq(e.what(), "login or password too long");
+//    }
+//}
