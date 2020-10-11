c87bb825746b561e97d1d080acdeaab731b68fce
BERNARD Damien [Hobbit - Encorpluptit] <damien.bernard@epitech.eu>
2020-10-11 06:51:31 +0200

    fix(windows): Fixing server on Windows.

--- BabelClient/main.cpp
+++ BabelClient/main.cpp
@@ -10,10 +10,15 @@
 
 int main(int ac, char **av)
 {
+    std::cout << "ici" << std::endl;
     if (ac != 3)
         return (84);
+    std::cout << "ici" << std::endl;
     BabelNetwork::NetworkInfos nwi(av[1], av[2]);
+    std::cout << "ici" << std::endl;
     NetworkClientSocket network(nwi.getIp(), nwi.getPort());
+    std::cout << "ici" << std::endl;
     BabelClient::ClientCore client(ac, av, network);
+    std::cout << "ici" << std::endl;
     return (client.run());
 }
